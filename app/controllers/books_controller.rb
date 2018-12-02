b13acf2fd672bb8b0aec1e9ae9c6a61ec3bf171c
Lukegits <luket.chavez@gmail.com>
2018-12-02 10:50:54 -0700

    added basic edit functionality

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,30 @@
 class BooksController < ApplicationController
-
+before_action :find_book, only: [:show, :edit, :update, :destroy]
   def index
+   @books = Book.all.order("created_at DESC")
+  end
+
+  def show
   end
 
   def new
     @book = Book.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @book.destroy
+    redirect_to root_path
   end
 
   def create
@@ -20,5 +40,8 @@ class BooksController < ApplicationController
 
     def book_params
       params.require(:book).permit(:title, :description, :author)
+    end
+    def find_book
+     @book = Book.find(params[:id])
     end
 end
