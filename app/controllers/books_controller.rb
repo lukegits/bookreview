a70e3d88b04d4e5583446f20af761d44f3dbbd8b
Lukegits <luket.chavez@gmail.com>
2018-12-03 18:51:29 -0700

    added categories

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,12 @@
 class BooksController < ApplicationController
 before_action :find_book, only: [:show, :edit, :update, :destroy]
   def index
-   @books = Book.all.order("created_at DESC")
+    if params[:category].blank?
+      @books = Book.all.order("created_at DESC")
+    else
+      @category_id = Category.find_by(name: params[:category]).id
+      @books = Book.where(:category_id => @category_id).order("create_at DESC")
+    end
   end
 
   def show
@@ -9,12 +14,15 @@ before_action :find_book, only: [:show, :edit, :update, :destroy]
 
   def new
     @book = current_user.books.build
+    @categories = Category.all.map{ |c| [c.name, c.id]}
   end
 
   def edit
+    @categories = Category.all.map{ |c| [c.name, c.id]}
   end
 
   def update
+    @book.category_id = params[:category_id]
     if @book.update(book_params)
       redirect_to book_path(@book)
     else
@@ -29,6 +37,7 @@ before_action :find_book, only: [:show, :edit, :update, :destroy]
 
   def create
     @book = current_user.books.build(book_params)
+    @book.category_id = params[:category_id]
     if @book.save
       redirect_to root_path
     else
@@ -39,7 +48,7 @@ before_action :find_book, only: [:show, :edit, :update, :destroy]
   private
 
     def book_params
-      params.require(:book).permit(:title, :description, :author)
+      params.require(:book).permit(:title, :description, :author, :category_id)
     end
     def find_book
      @book = Book.find(params[:id])
